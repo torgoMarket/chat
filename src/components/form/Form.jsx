8f6f375ff2b7203090fa1ab71ef4603e22c01002
Amir <torgoMarket@gmail.com>
2023-07-21 10:49:25 +0500

    new design
    -new design (light theme)
    -fix bug when all chats was deleted

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -4,6 +4,7 @@ import { addUser } from "../../store/usersSlice";
 import styles from "./Form.module.scss"
 import { useNavigate } from "react-router-dom";
 import { setAuthUser } from "../../store/authUserSlice";
+import formImage from "../../images/formImage.jpg"
 
 const Form = () => {
   const router = useNavigate()
@@ -51,24 +52,29 @@ const Form = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className={styles.form}>
-      <h3>Sign Up/ Login</h3>
-      <input
-        placeholder="userName"
-        className={styles.input}
-        type="text"
-        value={userName}
-        onChange={(e) => setUserName(e.target.value)}
-      />
-      <input
-        placeholder="password"
-        className={styles.input}
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button className={styles.button} type="submit">login</button>
-    </form>
+    <div className={styles.form}>
+      <div className={styles.image}>
+        <img src={formImage} alt="" />
+      </div>
+      <form onSubmit={handleSubmit}>
+        <h3>Sign Up/ Login</h3>
+        <input
+          placeholder="userName"
+          className={styles.input}
+          type="text"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <input
+          placeholder="password"
+          className={styles.input}
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className={styles.button} type="submit">login</button>
+      </form>
+    </div>
   );
 };
 
