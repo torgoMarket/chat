7efc26106a865c6a6fd93a317976640d155283a7
Amir <torgoMarket@gmail.com>
2023-08-14 19:58:15 +0500

    Update Main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import Form from '../components/form/Form'
 
 const Main = () => {
-  return (
-    <div className='mainPage'>
-        <Form></Form>
-    </div>
-  )
+	return (
+		<div className='mainPage'>
+			<Form></Form>
+		</div>
+	)
 }
 
 export default Main
